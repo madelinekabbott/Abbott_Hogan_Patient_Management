1dcbf127af96c6c6bf528ab0824eca7f020794f3
BenRHogan <160977910+BenRHogan@users.noreply.github.com>
2025-03-12 18:43:00 -0400

    Update login.php

--- 1_code/login.php
+++ 1_code/login.php
@@ -7,7 +7,7 @@ include 'header_no_dash.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
-    <link rel="stylesheet" href="patient_management_style.css">
+    <link rel="stylesheet" href="student_management_style.css">
     <script>
         function showErrorPopup(message) {
             alert(message);
@@ -18,7 +18,7 @@ include 'header_no_dash.php';
     <div class="container">
         <h2>Log In</h2>
         <form action="authenticate.php" method="POST">
-            <label for="username">User ID (Doctor/Admin):</label>
+            <label for="username">User ID (Tutor/Admin):</label>
             <input type="text" id="username" name="username" required><br><br>
 
             <label for="password">Password:</label>
@@ -26,7 +26,7 @@ include 'header_no_dash.php';
 
             <label for="role">Role:</label>
             <select id="role" name="role" required>
-                <option value="doctor">Doctor</option>
+                <option value="tutor">Tutor</option>
                 <option value="admin">Admin</option>
             </select><br><br>
 
@@ -35,7 +35,7 @@ include 'header_no_dash.php';
     </div>
     <?php
     if (isset($_GET['error'])) {
-        if ($_GET['error'] === 'doctor') {
+        if ($_GET['error'] === 'tutor') {
             echo "<script>showErrorPopup('Invalid credentials. If you are having trouble logging in, please contact an admin to reset your password.');</script>";
         } elseif ($_GET['error'] === 'admin') {
             echo "<script>showErrorPopup('Invalid credentials. If you need to reset your password, please contact the developer.');</script>";
