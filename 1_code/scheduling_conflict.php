a1f8c7b41b80d68840118528a435f1c33d0671dd
BenRHogan <160977910+BenRHogan@users.noreply.github.com>
2025-03-12 19:39:49 -0400

    Update scheduling_conflict.php

--- 1_code/scheduling_conflict.php
+++ 1_code/scheduling_conflict.php
@@ -4,9 +4,9 @@ require 'db_connect.php';
 include 'header.php';
 
 $isAdmin = isset($_SESSION['admin_id']);
-$isDoctor = isset($_SESSION['doctor_id']);
+$isTutor = isset($_SESSION['tutor_id']);
 
-if (!$isAdmin && !$isDoctor) {
+if (!$isAdmin && !$isTutor) {
     header("Location: login.php");
     exit();
 }
@@ -19,12 +19,12 @@ $scheduleUrl = $isAdmin ? "admin_schedule_appointment.php" : "schedule_appointme
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="patient_management_style.css">
+    <link rel="stylesheet" href="student_management_style.css">
 </head>
 <body>
     <div class="container">
         <h2>Scheduling Conflict</h2>
-        <p class="text">The appointment cannot be scheduled due to a scheduling conflict. Ensure that neither the doctor nor patient have appointments closely before or after the appointment you're scheduling.</p>
+        <p class="text">The appointment cannot be scheduled due to a scheduling conflict. Ensure that neither the tutor nor student have appointments closely before or after the appointment you're scheduling.</p>
         <div class="buttons">
             <a href="<?php echo $scheduleUrl; ?>" class="button">Try Again</a>
         </div>
