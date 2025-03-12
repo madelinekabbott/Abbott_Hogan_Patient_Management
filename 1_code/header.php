d00ed737521f3937d08f151cb4a0e3d78962fc51
BenRHogan <160977910+BenRHogan@users.noreply.github.com>
2025-03-12 18:33:22 -0400

    Update header.php

--- 1_code/header.php
+++ 1_code/header.php
@@ -5,8 +5,8 @@ $dashboardUrl = 'login.php';
 if (isset($_SESSION['admin_id'])) {
     $dashboardUrl = 'admin_dashboard.php';
 }
-elseif (isset($_SESSION['doctor_id'])) {
-    $dashboardUrl = 'doctor_dashboard.php';
+elseif (isset($_SESSION['tutor_id'])) {
+    $dashboardUrl = 'tutor_dashboard.php';
 }
 ?>
 <!DOCTYPE html>
@@ -14,8 +14,8 @@ elseif (isset($_SESSION['doctor_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patient Management System</title>
-    <link rel="stylesheet" href="patient_management_style.css">
+    <title>Tutor Management System</title>
+    <link rel="stylesheet" href="tutor_management_style.css">
 </head>
 <body>
     <header>
@@ -24,7 +24,7 @@ elseif (isset($_SESSION['doctor_id'])) {
                 <img src="https://vectorified.com/images/home-icon-png-white-33.png" alt="Dashboard Icon" class="dashboard-img">
             </a> 
         </nav>
-        <h1>Patient Management System</h1>
+        <h1>Tutor Management System</h1>
     </header>
 </body>
 </html>
