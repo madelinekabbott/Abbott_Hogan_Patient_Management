29b43f4602b824505c79cfce74811725b9d5c2b2
BenRHogan <160977910+BenRHogan@users.noreply.github.com>
2025-03-12 18:41:56 -0400

    Update header_no_dash.php

--- 1_code/header_no_dash.php
+++ 1_code/header_no_dash.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patient Management System</title>
-    <link rel="stylesheet" href="patient_management_style.css"> 
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="student_management_style.css"> 
 </head>
 <body>
     <header>
         <nav></nav>
-        <h1>Patient Management System</h1>
+        <h1>Student Management System</h1>
     </header>
 </body>
 </html>
