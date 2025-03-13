574f4d290602b020ae3f9d980fcd905e3c323b2c
BenRHogan <160977910+BenRHogan@users.noreply.github.com>
2025-03-13 17:38:01 -0400

    Update submit_appointment.php

--- 1_code/submit_appointment.php
+++ 1_code/submit_appointment.php
@@ -23,11 +23,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             $stmt = $pdo->prepare(
                 "SELECT * FROM (
-                    SELECT StudentID, TutorID, CheckTime AS StartTime, DATE_ADD(CheckTime, INTERVAL 30 MINUTE) AS EndTime FROM CheckUp
+                    SELECT StudentID, TutorID, MeetUpTime AS StartTime, DATE_ADD(MeetUpTime, INTERVAL 30 MINUTE) AS EndTime FROM MeetUp
                     UNION
-                    SELECT StudentID, TutorID, SurgeryTime AS StartTime, DATE_ADD(SurgeryTime, INTERVAL 120 MINUTE) AS EndTime FROM Surgery
+                    SELECT StudentID, TutorID, HomeworkHelpTime AS StartTime, DATE_ADD(HomeworkHelpTime, INTERVAL 120 MINUTE) AS EndTime FROM HomeworkHelp
                     UNION
-                    SELECT StudentID, NULL AS TutorID, LabTime AS StartTime, DATE_ADD(LabTime, INTERVAL 30 MINUTE) AS EndTime FROM Labs
+                    SELECT StudentID, NULL AS TutorID, TestPrepTime AS StartTime, DATE_ADD(TestPrepTime, INTERVAL 30 MINUTE) AS EndTime FROM TestPrep
                 ) AS Appointments
                 WHERE (StudentID = ? OR TutorID = ?) AND StartTime < ? AND EndTime > ?"
             );
@@ -36,52 +36,52 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             return $stmt->rowCount() > 0;
         }
 
-        if ($appointment_type === 'surgery') {
-            $surgery_time = $_POST['surgery_time'];
-            $surgery_type = $_POST['surgery_type'];
+        if ($appointment_type === 'homeworkHelp') {
+            $Hw_time = $_POST['Hw_time'];
+            $Hw_type = $_POST['Hw_type'];
 
-            if ($surgery_time < $current_time) {
-                throw new Exception("Cannot schedule a surgery in the past.");
+            if ($Hw_time < $current_time) {
+                throw new Exception("Cannot schedule homework help in the past.");
             }
 
-            if (isScheduleConflict($pdo, $student_id, $tutor_id, $surgery_time, 120)) {
+            if (isScheduleConflict($pdo, $student_id, $tutor_id, $Hw_time, 120)) {
                 header("Location: scheduling_conflict.php");
                 exit();
             }
 
-            $stmt = $pdo->prepare("INSERT INTO Surgery (StudentID, SurgeryTime, SurgeryType, TutorID) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$student_id, $surgery_time, $surgery_type, $tutor_id]);
+            $stmt = $pdo->prepare("INSERT INTO HomeworkHelp (StudentID, HwTime, HwType, TutorID) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$student_id, $Hw_time, $Hw_type, $tutor_id]);
         } elseif ($appointment_type === 'lab') {
-            $lab_time = $_POST['lab_time'];
-            $lab_type = $_POST['lab_type'];
-            $clinic_location = $_POST['clinic_location'];
+            $testprep_time = $_POST['testprep_time'];
+            $testprep_type = $_POST['testprep_type'];
+            $tutor_location = $_POST['tutor_location'];
 
-            if ($lab_time < $current_time) {
-                throw new Exception("Cannot schedule a lab in the past.");
+            if ($testprep_time < $current_time) {
+                throw new Exception("Cannot schedule test prep in the past.");
             }
 
-            if (isScheduleConflict($pdo, $student_id, null, $lab_time, 30)) {
+            if (isScheduleConflict($pdo, $student_id, null, $testprep_time, 30)) {
                 header("Location: scheduling_conflict.php");
                 exit();
             }
 
-            $stmt = $pdo->prepare("INSERT INTO Labs (StudentID, LabTime, LabType, ClinicLocation) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$student_id, $lab_time, $lab_type, $clinic_location]);
-        } elseif ($appointment_type === 'checkup') {
-            $checkup_time = $_POST['checkup_time'];
-            $checkup_reason = $_POST['checkup_reason'];
+            $stmt = $pdo->prepare("INSERT INTO Labs (StudentID, TestPrepTime, TestPrepType, TutorLocation) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$student_id, $testprep_time, $testprep_type, $tutor_location]);
+        } elseif ($appointment_type === 'meetup') {
+            $meetup_time = $_POST['meetup_time'];
+            $meetup_reason = $_POST['meetup_reason'];
 
-            if ($checkup_time < $current_time) {
+            if ($meetup_time < $current_time) {
                 throw new Exception("Cannot schedule a meeting in the past.");
             }
 
-            if (isScheduleConflict($pdo, $student_id, $tutor_id, $checkup_time, 30)) {
+            if (isScheduleConflict($pdo, $student_id, $tutor_id, $meetup_time, 30)) {
                 header("Location: scheduling_conflict.php");
                 exit();
             }
 
-            $stmt = $pdo->prepare("INSERT INTO CheckUp (StudentID, CheckTime, CheckReason, TutorID) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$student_id, $checkup_time, $checkup_reason, $tutor_id]);
+            $stmt = $pdo->prepare("INSERT INTO CheckUp (StudentID, MeetUpTime, MeetUpReason, TutorID) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$student_id, $meetup_time, $meetup_reason, $tutor_id]);
         }
 
         header("Location: success.php");
